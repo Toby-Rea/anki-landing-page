488a4d3d3beaa9180843ee5a848f09e1ae5537bd
Toby Rea <toby.e.rea@protonmail.com>
2024-09-10 21:05:15 +0100

    fix: padding to improve spacing on smaller screens

--- src/lib/components/common/LinkCard.svelte
+++ src/lib/components/common/LinkCard.svelte
@@ -11,7 +11,7 @@
   class="border-2 border-white/[13%] flex flex-col gap-6 justify-between xl:aspect-[5/6] px-14 py-9 hover:shadow-[0_5px_21px_rgba(150_,140_,168_,_0.4)] transition-all duration-75"
 >
   <div class="flex items-start justify-between">
-    <h2 class="text-xl font-medium uppercase xl:text-4xl">{title}</h2>
+    <h2 class="text-xl font-medium uppercase xl:text-4xl pr-2">{title}</h2>
     <img src="/icons/link-arrow.svg" alt="outlink" class="size-6 xl:size-8" />
   </div>
   <p class="text-neutral xl:text-xl">{description}</p>
