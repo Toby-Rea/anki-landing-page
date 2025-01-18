8363ac7d80107407d47048d194644846ebf4ffa9
ittipat <39715559+ittipatken@users.noreply.github.com>
2025-01-18 19:49:43 +0700

    fix: inappropriate margin on mobile

--- src/lib/components/common/LinkCard.svelte
+++ src/lib/components/common/LinkCard.svelte
@@ -10,7 +10,7 @@
   let { title, description, href }: LinkCard = $props();
 </script>
 
-<a {href} target="_blank" rel="noopener noreferrer" class="card group">
+<a {href} target="_blank" rel="noopener noreferrer" class="group space-y-10 px-9 py-6 hover:bg-gradient-to-r from-secondary/90 to-primary/90">
   <div class="flex items-start justify-between">
     <h2 class="flex flex-row items-center text-xl sm:text-2xl uppercase font-medium group-hover:text-background">
       {title}&nbsp;&nbsp;&nbsp;<img src="{base}/icons/link-arrow.svg" alt="outlink" class="size-6" />
@@ -21,8 +21,3 @@
   </p>
 </a>
 
-<style>
-  .card {
-    @apply flex flex-col border-white/[13%] justify-between space-y-10 col-span-2 last-of-type:col-span-2 first-of-type:border-t-0 border-t-2 px-9 py-6 sm:border-t-0 sm:first-of-type:border-r-2 sm:last-of-type:border-t-2 sm:col-span-1 md:first-of-type:border-r-0 md:last-of-type:border-t-0 md:last-of-type:col-span-1 hover:bg-gradient-to-r from-secondary/90 to-primary/90;
-  }
-</style>
