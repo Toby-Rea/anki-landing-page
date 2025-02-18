0593e4a662fc11aa5730cd49c2fc376881cceb76
Toby Rea <toby.e.rea@protonmail.com>
2025-02-18 08:34:48 +0000

    refactor: container queries, wording changes and styles

--- src/lib/components/sections/Hero.svelte
+++ src/lib/components/sections/Hero.svelte
@@ -2,40 +2,36 @@
   import { base } from '$app/paths';
 </script>
 
-<section class="flex items-center justify-between min-w-full py-20">
-  <div class="flex flex-col max-w-[768px] sm:pr-12 gap-14 xl:gap-20">
-    <div class="flex flex-col gap-9">
-      <h1
-        class="font-semibold text-4xl md:text-6xl 2xl:text-7xl 2xl:tracking-[1.78px] 2xl:leading-[89px]"
+<section class="flex items-center justify-between min-w-full @container">
+  <div class="grid size-full max-w-3xl @container py-[10cqw]">
+    <h1 class="font-semibold leading-none tracking-tight text-[max(11cqw,_2.5em)]">
+      Remembering is easier with <span class="text-primary">Anki</span>
+    </h1>
+    <p class="text-neutral tracking-tight leading-tight text-[max(3cqw,_1em)] mt-[max(4cqw,_1em)]">
+      Anki adapts to your learning pace so you spend more time on challenging material and less on what you already know.
+    </p>
+    <div class="grid grid-cols-2 w-full min-w-[60cqw] max-w-xl h-[max(9cqw,_3em)] mt-[max(6cqw,_1.25em)]">
+      <a
+        href="#downloads"
+        onclick={(event) => {
+          event.preventDefault();
+          document.querySelector('#downloads')?.scrollIntoView({ behavior: 'smooth' });
+        }}
+        class="hover:opacity-80 h-full font-bold rounded bg-gradient-to-r from-primary-darker to-primary text-background flex justify-center items-center text-[max(3cqw,_1em)] transition-opacity duration-300 ease-out"
       >
-        Make learning easier with <span class="text-primary">Anki</span>
-      </h1>
-      <p class="text-xl md:text-3xl text-neutral tracking-[1.02px]">
-        The flashcard application that makes sure you only practice the things you're about to
-        forget.
-      </p>
-    </div>
-    <div class="flex flex-wrap items-center gap-5 sm:gap-9">
-      <div class="bg-secondary">
-        <a
-          href="#downloads"
-          onclick={(event) => {
-            event.preventDefault();
-            document.querySelector('#downloads')?.scrollIntoView({ behavior: 'smooth' });
-          }}
-          class="bg-primary aspect-16/5 w-40 md:w-56 px-3 lg:px-8 border-2 border-primary text-background flex justify-center items-center text-lg md:text-xl font-bold hover:opacity-80 transition-all duration-200"
-        >
-          Download
-        </a>
-      </div>
+        Download Anki
+      </a>
       <a
         href="https://docs.ankiweb.net/background.html"
         aria-label="Anki Manual: Background"
         target="_blank"
         rel="noopener noreferrer"
-        class="aspect-16/5 w-40 md:w-56 px-3 lg:px-8 text-black dark:text-white border-2 border-black/[13%] dark:border-white/[13%] flex justify-center items-center text-lg md:text-xl font-bold hover:opacity-60 transition-all duration-200"
+        class="group hover:opacity-80 flex items-center gap-2 justify-center h-full font-bold text-foreground text-[max(3cqw,_1em)] transition-opacity duration-300 ease-out"
       >
-        + Learn more
+        Learn more
+        <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 24 24" fill="currentColor" class="size-5 group-hover:translate-x-1 transition-transform duration-300 ease-out">
+          <path fill-rule="evenodd" d="M12.97 3.97a.75.75 0 0 1 1.06 0l7.5 7.5a.75.75 0 0 1 0 1.06l-7.5 7.5a.75.75 0 1 1-1.06-1.06l6.22-6.22H3a.75.75 0 0 1 0-1.5h16.19l-6.22-6.22a.75.75 0 0 1 0-1.06Z" clip-rule="evenodd" />
+        </svg>
       </a>
     </div>
   </div>
