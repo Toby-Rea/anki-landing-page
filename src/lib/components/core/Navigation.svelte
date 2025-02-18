a76ff211b2ecb3cb2ff7376a9c7b867cc5f33dc0
Toby Rea <toby.e.rea@protonmail.com>
2025-02-18 08:56:29 +0000

    fix: code formatting and border colour for light mode

--- src/lib/components/core/Navigation.svelte
+++ src/lib/components/core/Navigation.svelte
@@ -14,7 +14,7 @@
 </script>
 
 <header
-  class="bg-background/90 backdrop-blur-lg sticky inset-x-0 top-0 z-10 border-b border-white/[13%]"
+  class="bg-background/90 backdrop-blur-lg sticky inset-x-0 top-0 z-10 border-b border-foreground/[13%]"
 >
   <div class="max-w-7xl mx-auto">
     <div class="flex h-16 items-center justify-between gap-8 px-6">
@@ -24,9 +24,7 @@
           class="flex items-center gap-2 shrink-0 [&>*]:pointer-events-none [&>*]:select-none"
         >
           <img src="{base}/logo.svg" alt="Logo" class="size-7" />
-          <span
-            class="font-semibold text-foreground text-xl leading-none tracking-wider"
-          >
+          <span class="font-semibold text-foreground text-xl leading-none tracking-wider">
             ANKI
           </span>
         </a>
@@ -40,11 +38,12 @@
             {text}
           </a>
         {/each}
-        <a aria-label="GitHub repository" href="https://github.com/ankitects/anki" class="px-3 py-2 group">
-          <svg
-            viewBox="0 0 20 20"
-            class="size-5 fill-foreground group-hover:fill-primary"
-          >
+        <a
+          aria-label="GitHub repository"
+          href="https://github.com/ankitects/anki"
+          class="px-3 py-2 group"
+        >
+          <svg viewBox="0 0 20 20" class="size-5 fill-foreground group-hover:fill-primary">
             <path
               d="M10 0C4.475 0 0 4.475 0 10a9.994 9.994 0 006.838 9.488c.5.087.687-.213.687-.476 0-.237-.013-1.024-.013-1.862-2.512.463-3.162-.612-3.362-1.175-.113-.287-.6-1.175-1.025-1.412-.35-.188-.85-.65-.013-.663.788-.013 1.35.725 1.538 1.025.9 1.512 2.337 1.087 2.912.825.088-.65.35-1.088.638-1.338-2.225-.25-4.55-1.112-4.55-4.937 0-1.088.387-1.987 1.025-2.688-.1-.25-.45-1.274.1-2.65 0 0 .837-.262 2.75 1.026a9.28 9.28 0 012.5-.338c.85 0 1.7.112 2.5.337 1.912-1.3 2.75-1.024 2.75-1.024.55 1.375.2 2.4.1 2.65.637.7 1.025 1.587 1.025 2.687 0 3.838-2.337 4.688-4.562 4.938.362.312.675.912.675 1.85 0 1.337-.013 2.412-.013 2.75 0 .262.188.574.688.474A10.016 10.016 0 0020 10c0-5.525-4.475-10-10-10z"
             >
@@ -53,7 +52,8 @@
         </a>
         <a
           href="#downloads"
-          class="ml-3 rounded hover:opacity-80 bg-gradient-to-r from-primary-darker to-primary text-background h-12 flex items-center px-4 font-semibold leading-none cursor-pointer transition-all duration-300 ease-out">
+          class="ml-3 rounded hover:opacity-80 bg-gradient-to-r from-primary-darker to-primary text-background h-12 flex items-center px-4 font-semibold leading-none cursor-pointer transition-all duration-300 ease-out"
+        >
           Download Anki
         </a>
       </div>
@@ -125,8 +125,11 @@
             {/each}
             <a
               href="#downloads"
-              onclick={() => {document.getElementById('menu')?.hidePopover()}}
-              class="text-center mt-auto rounded hover:opacity-80 bg-gradient-to-r from-primary-darker to-primary text-background py-4 text-lg flex items-center justify-center font-semibold leading-none cursor-pointer transition-all duration-300 ease-out">
+              onclick={() => {
+                document.getElementById('menu')?.hidePopover();
+              }}
+              class="text-center mt-auto rounded hover:opacity-80 bg-gradient-to-r from-primary-darker to-primary text-background py-4 text-lg flex items-center justify-center font-semibold leading-none cursor-pointer transition-all duration-300 ease-out"
+            >
               Download Anki
             </a>
           </nav>
