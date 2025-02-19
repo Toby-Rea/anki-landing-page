07a54af3044568ef43f04b9d121e49a3adc16857
ittipat <39715559+ittipatken@users.noreply.github.com>
2025-02-19 07:22:35 +0700

    feat: add color to titles

--- src/lib/components/common/DualHeader.svelte
+++ src/lib/components/common/DualHeader.svelte
@@ -10,7 +10,7 @@
 </script>
 
 <hgroup class="flex flex-col gap-3">
-  <h2 {id} class="text-2xl xl:text-4xl tracking-[1.65px] uppercase font-medium scroll-mt-32">
+  <h2 {id} class="text-2xl xl:text-4xl tracking-[1.65px] uppercase font-medium scroll-mt-32 text-transparent bg-linear-10 bg-clip-text from-primary-darker to-primary">
     {title}
   </h2>
   <div class="text-lg xl:text-3xl xl:tracking-[1.02px] xl:leading-[45px] text-neutral">
