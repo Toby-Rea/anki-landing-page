cb632bc760c90b58f940bc438ad488786de5ce12
Toby Rea <toby.e.rea@protonmail.com>
2025-02-20 01:28:21 +0000

    style: reduced margins and increased subtext

--- src/lib/components/common/DualHeader.svelte
+++ src/lib/components/common/DualHeader.svelte
@@ -12,10 +12,10 @@
 </script>
 
 <hgroup
-  class="my-20 mx-auto w-full flex flex-col text-center left items-center justify-center gap-4 px-4 md:max-w-[820px] scroll-mt-32 @container"
+  class="my-12 mx-auto w-full flex flex-col text-center left items-center justify-center gap-4 px-4 md:max-w-[820px] scroll-mt-32 @container"
 >
   <h2 {id} class="text-[max(6cqw,_1.875em)] font-semibold capitalize leading-none">{title}</h2>
-  <p class="text-[max(2.5cqw,_0.9375em)] text-subtle leading-snug">
+  <p class="text-[max(2.5cqw,_1em)] text-subtle leading-snug">
     {@render subtitle()}
   </p>
 </hgroup>
