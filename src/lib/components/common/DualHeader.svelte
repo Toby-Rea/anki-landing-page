27cd3b9a5a1bb029ab00e64e4690723a588e530d
Toby Rea <toby.e.rea@protonmail.com>
2025-02-22 16:32:27 +0000

    fix: parent needs to be a div

--- src/lib/components/common/DualHeader.svelte
+++ src/lib/components/common/DualHeader.svelte
@@ -15,7 +15,7 @@
   class="my-12 mx-auto w-full flex flex-col text-center left items-center justify-center gap-4 px-4 md:max-w-[820px] scroll-mt-32 @container"
 >
   <h2 {id} class="text-[max(6cqw,_1.875em)] font-semibold capitalize leading-none">{title}</h2>
-  <p class="text-[max(2.5cqw,_1em)] text-subtle leading-snug">
+  <div class="text-[max(2.5cqw,_1em)] text-subtle leading-snug">
     {@render subtitle()}
-  </p>
+  </div>
 </hgroup>
