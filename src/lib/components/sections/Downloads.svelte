831b009cf4275361387b25e90506d1e7264501e8
Damien Elmes <gpg@ankiweb.net>
2025-07-21 16:15:25 +0700

    Add discrete link to 25.07.3

--- src/lib/components/sections/Downloads.svelte
+++ src/lib/components/sections/Downloads.svelte
@@ -161,6 +161,25 @@
             />
           </svg>
         </a>
+        <a
+          href="https://github.com/ankitects/anki/releases/tag/25.07.3"
+          class="flex items-center gap-1 w-fit font-medium hover:opacity-60"
+          style="color: green;"
+          >Try the new July release <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            stroke-width="1.5"
+            stroke="currentColor"
+            class="size-4 stroke-2"
+          >
+            <path
+              stroke-linecap="round"
+              stroke-linejoin="round"
+              d="m4.5 19.5 15-15m0 0H8.25m11.25 0v11.25"
+            />
+          </svg>
+        </a>
       </div>
     </div>
     <div
