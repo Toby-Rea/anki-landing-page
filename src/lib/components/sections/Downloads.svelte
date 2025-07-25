465c4f8a5f124fdffa6dc44649eaeebafa51096b
Damien Elmes <gpg@ankiweb.net>
2025-07-26 00:12:36 +0700

    25.07.5

--- src/lib/components/sections/Downloads.svelte
+++ src/lib/components/sections/Downloads.svelte
@@ -1,7 +1,7 @@
 <script lang="ts">
   import DualHeader from '$lib/components/common/DualHeader.svelte';
 
-  const ankiVersion = '25.07.4';
+  const ankiVersion = '25.07.5';
   const winMinVer = '10';
   const macosMinVer = '12';
   const linuxMinYear = '2023';
